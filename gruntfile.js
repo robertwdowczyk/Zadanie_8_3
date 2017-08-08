c88bb8c7d567618f099c5106901285790f95d41b
robertw <r.wdowczyk@gmail.com>
2017-08-08 18:43:19 +0200

    zmiana

---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+﻿module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      all: ['js/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
